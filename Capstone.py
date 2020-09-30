7cfef7e9ebdeb4788ecce34205af9185747a71de
Michael Mathews Jr <56321595+MikeTD12@users.noreply.github.com>
2020-09-30 16:52:05 -0400

    Update Capstone.py

--- Capstone.py
+++ Capstone.py
@@ -9,7 +9,6 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-get_ipython().run_line_magic('matplotlib', 'inline')
 import seaborn as sns
 
 from sklearn.compose import make_column_transformer
